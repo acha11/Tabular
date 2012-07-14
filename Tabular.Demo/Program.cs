45aad012f1ad49fa01952b61a87e6187343ebcbb
Andrew Chalmers <ajchalmers@gmail.com>
2012-07-14 21:50:18 +1000

    Implemented CSV output

--- Tabular.Demo/Program.cs
+++ Tabular.Demo/Program.cs
@@ -13,7 +13,8 @@ namespace Tabular.Demo
 		{
 			RunDemo("Demo 1: Simple render to console",                     () => Demo1_DemonstrateSimpleRenderToConsole());
 			RunDemo("Demo 2: Simultaneous render to console and html file", () => Demo2_DemonstrateSimultaneousRenderToConsoleAndHtmlFile());
-			RunDemo("Demo 3: Simultaneous render to text and html files",   () => Demo3_DemonstrateSimultaneousRenderToConsoleAndTextFile());
+			RunDemo("Demo 3: Render to text file",                          () => Demo3_DemonstrateRenderToTextFile());
+			RunDemo("Demo 4: Render to csv file",                           () => Demo4_DemonstrateRenderToCsvFile());
 		}
 
 		private static void Demo1_DemonstrateSimpleRenderToConsole()
@@ -65,39 +66,48 @@ namespace Tabular.Demo
 			Process.Start("demo2Out.html");
 		}
 
-		private static void Demo3_DemonstrateSimultaneousRenderToConsoleAndTextFile()
+		private static void Demo3_DemonstrateRenderToTextFile()
 		{
-			using (StreamWriter demo3HtmlOut = new StreamWriter("demo3Out.html"))
+			// First, let's build a collection of objects to render.
+			var filesInCurrentDirectory =
+				new DirectoryInfo(Directory.GetCurrentDirectory()).GetFiles();
+
+			// The anonymous type we're building has properties Name, Extension and CreationTime.
+			// The resulting table will have a column for each of those properties.
+			var objectsToRender =
+				filesInCurrentDirectory
+				.Select(x => new { x.Name, x.Extension, x.CreationTime });
+
 			using (StreamWriter demo3TextOut = new StreamWriter("demo3Out.txt"))
 			{
-				// First, let's build a collection of objects to render.
-				var filesInCurrentDirectory =
-					new DirectoryInfo(Directory.GetCurrentDirectory()).GetFiles();
-
-				// The anonymous type we're building has properties Name, Extension and CreationTime.
-				// The resulting table will have a column for each of those properties.
-				var objectsToRender =
-					filesInCurrentDirectory
-					.Select(x => new { x.Name, x.Extension, x.CreationTime });
-
-				// Next, set up some table renderers. First, a writer that will write the
-				// table to the text file.
-				var textTableWriter = new TextTableWriter(demo3TextOut);
-
-				// Now, a writer that will write HTML to an output file.
-				var htmlTableWriter = new HtmlTableWriter(demo3HtmlOut);
-
-				// Now, a single renderer which takes care of writing our single table to
-				// both of the above destinations.
-				var multipleTableRenderer = new MultipleTargetTableWriter(textTableWriter, htmlTableWriter);
-
 				// Finally, we actually render the table
-				TableRenderer.Render(objectsToRender, multipleTableRenderer);
+				TableRenderer.Render(objectsToRender, new TextTableWriter(demo3TextOut));
 			}
 
-			// And display the generated files
-			Process.Start("demo3Out.html");
+			// And display the generated file
 			Process.Start("demo3Out.txt");
+		}
+
+		private static void Demo4_DemonstrateRenderToCsvFile()
+		{
+			// First, let's build a collection of objects to render.
+			var filesInCurrentDirectory =
+				new DirectoryInfo(Directory.GetCurrentDirectory()).GetFiles();
+
+			// The anonymous type we're building has properties Name, Extension and CreationTime.
+			// The resulting table will have a column for each of those properties.
+			var objectsToRender =
+				filesInCurrentDirectory
+				.Select(x => new { x.Name, x.Extension, x.CreationTime });
+
+			using (StreamWriter demo4Out = new StreamWriter("demo4Out.csv"))
+			{
+				// Finally, we actually render the table
+				TableRenderer.Render(objectsToRender, new CsvTableWriter(demo4Out));
+			}
+
+			// And display the generated file
+			Process.Start("demo4Out.csv");
 		}
 
 		private static void RunDemo(string title, Action demo)
---
+++ Tabular/CsvTableWriter.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace Tabular
+{
+	public class CsvTableWriter : ITableWriter
+	{
+		StreamWriter _sw;
+		TableStructure _structure;
+		TableColumn _firstColumn;
+
+		public CsvTableWriter(StreamWriter sw)
+		{
+			_sw = sw;
+		}
+
+		public void StartTable(TableStructure tableStructure)
+		{
+			_structure = tableStructure;
+			_firstColumn = tableStructure.ColumnGroups.First().Columns.First();
+
+			if (_structure.GetAllColumns().Any(c => c.Title.Length > 0))
+			{
+				_sw.WriteLine(string.Join(",", _structure.ColumnGroups.SelectMany(cg => cg.Columns).Select(c => c.Title).ToArray()));
+			}
+		}
+
+		public void EndTable()
+		{
+		}
+
+		public void StartRow()
+		{
+		}
+
+		public void EndRow()
+		{
+			_sw.WriteLine();
+		}
+
+		public void WriteCell(TableColumn column, string value)
+		{
+			if (column != _firstColumn)
+			{
+				_sw.Write(",");
+			}
+
+			_sw.Write(value);
+		}
+
+		private string GetCsvEscapedValue(string value)
+		{
+			// If the value contains a comma
+			if (value.Contains(','))
+			{
+				// We wrap the whole thing in double quotes, being sure to duplicate any double quotes it already contained.
+				return "\"" + value.Replace("\"", "\"\"") + "\"";
+			}
+			else
+			{
+				return value;
+			}
+		}
+	}
+}
